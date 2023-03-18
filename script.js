77f88b7134a84eafe0303feaf346ac230734b14f
Daia asojo <123811992+Daia2283@users.noreply.github.com>
2023-03-18 02:04:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,120 @@
+
+
+var playerRed ="R";
+var playerYellow="Y";
+var currPlayer= playerRed;
+ var currCol;
+
+var gameOver = false;
+var board;
+
+var rows=6;
+var cols =7;
+
+window.onload = function() {
+    setGame();
+}
+ function setGame() {
+    board = [];
+    currCol = [5,5,5,5,5,5,5]
+    for (let r = 0; r < rows; r++) {
+        let row =[];
+      for (let c = 0; c < cols; c++) {
+        row.push('');
+        let tile =document.createElement("div");
+        tile.id = r.toString() + "-" + c.toString();
+        tile.classList.add("tile");
+        tile.addEventListener("click", setPiece);
+        document.getElementById("board").append(tile);
+      }
+        board.push(row);
+    }
+ }
+ function setPiece() {
+  if (gameOver) {
+    return;
+  } 
+    let cords = this.id.split("-");
+  let r = parseInt(cords[0]);
+  let c = parseInt(cords[1]);
+
+ r = currCol [c];
+ if (r < 0){
+return;
+ }
+
+  board[r][c] = currPlayer;
+  let tile = document.getElementById(r.toString() + "-" + c.toString());
+if (currPlayer == playerRed) {
+  tile.classList.add("red-piece");
+  currPlayer = playerYellow;
+} else {
+  tile.classList.add("yellow-piece")
+  currPlayer= playerRed;
+}
+r -=1;
+currCol[c] = r; 
+
+checkWinner();
+}
+function checkWinner() {//to Know how many are we connecting
+   for (let r = 0; r < rows; r++) {
+   for (let c = 0; c < cols-3; c++) {
+    if (board[r][c] !='') {
+      if (board[r][c] == board[r][c+1] && board[r][c+1] ==board[r][c+2] && board[r][c+2] == board[r][c+3]) {
+        setWinner(r, c);
+        return;
+      }
+    }
+    
+   }
+    
+   } 
+  //  vertically 
+  for (let c = 0; c < cols; c++) {
+    for (let r = 0; r < rows-3; r++) {
+     if (board[r][c] !='') {
+       if (board[r][c] == board[r+1][c] && board[r+1][c] == board[r+2][c] && board[r+2][c] == board[r+3][c]){
+         setWinner(r,c); 
+         return;
+       }
+     }
+     
+    }
+     
+  }
+    // diagonally 
+    for (let r = 0; r < rows-3; r++) {
+      for (let c = 0; c < cols-3; c++) {
+if(board[r][c] !=''){
+if (board[r][c] == board[r+1][c+1] && board[r+1][c+1] == board[r+2][c+2] && board[r+2][c+2] == board[r+3][c+3]) {
+  setWinner(r,c); 
+  return;
+}
+}
+      }
+      
+    }
+    // anti diagonal
+    for (let r = 3; r < rows; r++) {
+      for (let c = 0; c < cols-3; c++) {
+if(board[r][c] !=''){
+if (board[r][c] == board[r-1][c+1] && board[r-1][c+1] == board[r-2][c+2] && board[r-2][c+2] == board[r-3][c+3]) {
+  setWinner(r,c); 
+  return;
+}
+}
+      }
+      
+    }
+}
+
+function setWinner(r, c) {
+  let winner = document.getElementById("winner");
+  if (board[r][c] == playerRed) {
+    winner.innerText = "Red Wins";
+  }else{
+    winner.innerText = "Yellow Wins"
+  }
+  gameOver =true;
+}
